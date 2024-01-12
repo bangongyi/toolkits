d460b689f55c79b23e9b304cb114f5e7990d901d
zhangshaohua <zhangshaohua@tianjike.top>
2024-01-12 11:25:16 +0800

    Convert excel to text

--- office/officeToWord.go
+++ office/officeToWord.go
@@ -4,12 +4,22 @@ import (
 	"github.com/tealeg/xlsx"
 	"io/ioutil"
 	"log"
+	"strings"
 )
 
-func ExcelToWord(location string) (word string, err error) {
-	xlFile, err := xlsx.OpenFile(location)
+// excel文件转文字
+func ExcelToWord(filePath string) (word string, fileSuffix string, FileSize int, err error) {
+	suffix, err := getSuffix(filePath)
 	if err != nil {
-		return "", err
+		return "", "", 0, err
+	}
+	size, err := countSize(filePath)
+	if err != nil {
+		return "", "", 0, err
+	}
+	xlFile, err := xlsx.OpenFile(filePath)
+	if err != nil {
+		return "", "", 0, err
 	}
 
 	text := ""
@@ -21,15 +31,87 @@ func ExcelToWord(location string) (word string, err error) {
 			text += ""
 		}
 	}
-	return text, nil
+	return text, suffix, size, nil
 }
 
-func TxtToWord(location string) (word string, err error) {
-	content, err := ioutil.ReadFile(location)
+// excel地址文件转文字
+func ExcelUrlToWord(url string) (word string, fileSuffix string, FileSize int, err error) {
+	suffix, err := getSuffix(url)
+	if err != nil {
+		return "", "", 0, err
+	}
+
+	filePath, err := saveFile(url, suffix)
+	if err != nil {
+		return
+	}
+
+	size, err := countSize(filePath)
+	if err != nil {
+		return "", "", 0, err
+	}
+
+	xlFile, err := xlsx.OpenFile(filePath)
+	if err != nil {
+		return "", "", 0, err
+	}
+
+	text := ""
+	for _, sheet := range xlFile.Sheets {
+		for _, row := range sheet.Rows {
+			for _, cell := range row.Cells {
+				text += cell.String() + " "
+			}
+			text += ""
+		}
+	}
+
+	return text, suffix, size, nil
+}
+
+// txt文件转文字
+func TxtToWord(filePath string) (word string, fileSuffix string, FileSize int, err error) {
+	suffix, err := getSuffix(filePath)
+	if err != nil {
+		return "", "", 0, err
+	}
+	size, err := countSize(filePath)
+	if err != nil {
+		return "", "", 0, err
+	}
+	content, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	text := string(content)
+	text = strings.Replace(text, "\n", " ", -1)
 
-	return text, nil
+	return text, suffix, size, nil
+}
+
+// txt地址文件转文字
+func TxtUrlToWord(url string) (word string, fileSuffix string, FileSize int, err error) {
+	suffix, err := getSuffix(url)
+	if err != nil {
+		return "", "", 0, err
+	}
+
+	filePath, err := saveFile(url, suffix)
+	if err != nil {
+		return
+	}
+
+	size, err := countSize(filePath)
+	if err != nil {
+		return "", "", 0, err
+	}
+
+	content, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	text := string(content)
+	text = strings.Replace(text, "\n", " ", -1)
+
+	return text, suffix, size, nil
 }
